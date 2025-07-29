8835dd603940b77449f071c34451e75f5a1d90a2
vishal <vt3630@gmail.com>
2025-07-29 06:13:55 +0530

    second

--- munna/munna.py
+++ munna/munna.py
@@ -2,17 +2,9 @@ import sys
 from logic import generate_word
 
 def main():
-    # u_input = input("Hello from mumma!!!! my limit is 999999 \n" 
-    # "Enter the number: ")
+    u_input = input("Hello from mumma!!!! my limit is 999999 \n" 
+    "Enter the number: ")
     
-    # print(f"{u_input}: {generate_word(u_input)}")
-
-    number = 19
-    expected_output = []
-
-    for i in range(number):
-        expected_output.append(generate_word(str(i)))
-
-    print(expected_output)
+    print(f"{u_input}: {generate_word(u_input)}")
 
 sys.exit(main())
--- munna/test.py
+++ munna/test.py
@@ -1,5 +1,12 @@
 from logic import generate_word
 
-
+# it - is incomplete 
 def accuracy(insert_begin , insert_end):
+    number = 19
+    expected_output = []
+
+    for i in range(number):
+        expected_output.append(generate_word(str(i)))
+
+    print(expected_output)
     
